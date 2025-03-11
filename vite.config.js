a2099c5c1b37c2ba7ee1f0cfbdf2e65dead4799e
zak-info <z.benslite@esi-sba.dz>
2025-03-11 17:57:56 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Allows external access
+    port: 5173, // Ensure it's the correct port
+    strictPort: true,
+    hmr: {
+      clientPort: 5173,
+    },
+    allowedHosts: ['langos.chananee.com'], // Allow your domain
+  },
   plugins: [react()],
 })
